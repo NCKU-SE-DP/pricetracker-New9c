c232d51451c3756ea2bc2114d68d2e5a5470ef05
9c <ericgwhuang@gmail.com>
2024-12-19 18:29:14 +0800

    Added even more logs and exceptions

--- backend/src/news/router.py
+++ backend/src/news/router.py
@@ -13,22 +13,31 @@ router = APIRouter(prefix="/api/v1/news")
 
 @router.get("/news")
 def read_news(db: DatabaseSession):
+    logging.debug("Accessed /api/v1/news/news")
     return service.retrieve_news_with_upvote_status(db, None)
 
 
 @router.get("/user_news")
 def read_user_news(db: DatabaseSession, user: CurrentLoggedInUser):
+    logging.debug("Accessed /api/v1/news/user_news")
     return service.retrieve_news_with_upvote_status(db, user)
 
 
 
 @router.post("/search_news")
 async def search_news(request: PromptRequest):
+    logging.debug(f"Accessed /api/v1/news/search_news with prompt: {request.prompt}")
     return service.search_news(request.prompt)
 
 @router.post("/news_summary")
-async def news_summary( news: NewsSumaryRequestSchema, user: CurrentLoggedInUser):
-    summary = service.summarize_news(news.content)
+async def news_summary(news: NewsSumaryRequestSchema, user: CurrentLoggedInUser):
+    logging.debug("Accessed /api/v1/news/news_summary")
+    try:
+        summary = service.summarize_news(news.content)
+    except InvalidAIModelException as e:
+        logging.error("Invalid model used")
+        capture_exception(e)
+        return HTTPException(status_code=400, detail="Passed in invalid LLM model")
     response = {}
     if summary:
         response["summary"] = summary["影響"]
